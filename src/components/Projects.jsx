b9c46f17e71618ebd79eb0053260d43861ce81df
Sanketh-Ganiga <sankethganiga888@gmail.com>
2023-06-13 00:50:12 +0530

    projects changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 export const Projects = () => {
   return (
     <div id="projects">
-      <div>
-        <img src="" alt="" />
-        <h2 className='project-title'></h2>
-        <p className='project-description'></p>
-        <p className='project-tech-stack' ></p>
-        <p className='project-github-link'></p>
-        <p className='project-deployed-link'></p>
-      </div>
-      <div>
+      <div className='project-card'>
         <img src="" alt="" />
         <h2 className='project-title'>Project1</h2>
         <p className='project-description'></p>
@@ -19,7 +11,7 @@ export const Projects = () => {
         <p className='project-github-link'></p>
         <p className='project-deployed-link'></p>
       </div>
-      <div>
+      <div className='project-card'>
         <img src="" alt="" />
         <h2 className='project-title'>Project2</h2>
         <p className='project-description'></p>
@@ -27,7 +19,7 @@ export const Projects = () => {
         <p className='project-github-link'></p>
         <p className='project-deployed-link'></p>
       </div>
-      <div>
+      <div className='project-card'>
         <img src="" alt="" />
         <h2 className='project-title'>Project3</h2>
         <p className='project-description'></p>
@@ -35,7 +27,7 @@ export const Projects = () => {
         <p className='project-github-link'></p>
         <p className='project-deployed-link'></p>
       </div>
-      <div>
+      <div className='project-card'>
         <img src="" alt="" />
         <h2 className='project-title'>Project4</h2>
         <p className='project-description'></p>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,18 +21,18 @@ export const Skills=()=>{
           <div id="skills-container">
             {/* ------------------------------ */}
                <div className="level1">
-                <div>
+                <div className="skills-card">
                  <img src={react} alt="" className="skills-card-img" />
                  <p className="skills-card-name">ReactJS</p>
                 </div>
                </div>
                {/* ---------------------------------------- */}
                <div className="level2">
-                <div>
+                <div className="skills-card">
                   <img src={nodejs} alt="" className="skills-card-img" />
                   <p className="skills-card-name">NodeJS</p>
                </div>
-                  <div>
+                  <div className="skills-card">
                    <img src={mongodb} alt="" className="skills-card-img" />
                     <p className="skills-card-name">MongoDB</p>
                   </div>
@@ -40,15 +40,15 @@ export const Skills=()=>{
 
 
     <div className="level3">
-     <div>
+     <div className="skills-card">
      <img src={redux} alt="" className="skills-card-img" />
          <p className="skills-card-name">Redux</p>
      </div>
-    <div>
+    <div className="skills-card">
     <img src={express} alt="" className="skills-card-img" />
         <p className="skills-card-name">Express</p>
     </div>
-    <div>
+    <div className="skills-card">
     <img src={chakraui} alt="" className="skills-card-img" />
         <p className="skills-card-name">Chakra UI</p>
     </div>
@@ -56,19 +56,19 @@ export const Skills=()=>{
 
 
     <div className="level4">
-    <div>
+    <div className="skills-card">
         <img src={html} alt="" className="skills-card-img" />
         <p className="skills-card-name">HTML</p>
     </div>
-    <div>
+    <div className="skills-card">
     <img src={css} alt="" className="skills-card-img" />
         <p className="skills-card-name">CSS</p>
     </div>
-    <div>
+    <div className="skills-card">
     <img src={javascript} alt="" className="skills-card-img" />
         <p className="skills-card-name">JavaScript</p>
     </div>
-    <div>
+    <div className="skills-card">
     <img src={git} alt="" className="skills-card-img" />
         <p className="skills-card-name">Git</p>
     </div>
