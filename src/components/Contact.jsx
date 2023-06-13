29635da1dffe00fd453b4d8d430dbc2cb7a8ba89
Sanketh-Ganiga <sankethganiga888@gmail.com>
2023-06-13 11:32:08 +0530

    contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Contact = () => {
   return (
-    <div id="contact">Contact</div>
+    <div id="contact">
+      <h1>Contact Me</h1>
+      <a href="https://github.com/SankethGaniga95" target="_blank" id="contact-github">Github</a>
+      <a href="https://www.linkedin.com/in/sanketh-ganiga-35398317a/" target="_blank" id="contact-linkedin">Linked In</a>
+      <p id="contact-phone">Phone:+91 9892797599</p>
+      <a href="mailto:sankethganiga888@gmail.com" target='_blank' id="contact-email">Email</a>
+    </div>
   )
 }
