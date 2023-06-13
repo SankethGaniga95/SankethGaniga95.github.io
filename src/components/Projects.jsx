a9c528206195b710152e9f4eaff217c373a377d5
Sanketh-Ganiga <sankethganiga888@gmail.com>
2023-06-13 11:04:33 +0530

    projects changed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,39 +1,89 @@
 import React from 'react'
+import kiranawaale from "../images/kiranawaale.png"
+import mountaineer from "../images/Mountaineer.png"
+import achiever from "../images/Achiever_Avenue.png"
+import "./Projects.css"
 
 export const Projects = () => {
   return (
     <div id="projects">
+      <h1>Projects</h1>
       <div className='project-card'>
-        <img src="" alt="" />
-        <h2 className='project-title'>Project1</h2>
-        <p className='project-description'></p>
-        <p className='project-tech-stack' ></p>
-        <p className='project-github-link'></p>
-        <p className='project-deployed-link'></p>
+        <img src={achiever} alt="" />
+        <div className='project-details'>
+        <h2 className='project-title'>Achiever's Avenue</h2>
+        <p className='project-description'>Achiever's Avenue is a learning platform for the aspirants of IIT and Medical Colleges. It offers online and offline coaching.</p>
+        <div className='project-tech-stack'>
+        <h1>Tech Stack:-</h1>
+        <p>React</p>
+        <p>Chakra UI</p>
+        <p>JSON server</p>
+        </div>
+        <a href="https://github.com/SankethGaniga95/imaginary-blood-7556" target='_blank' className='project-github-link'>
+        <button >GitHub</button>
+        </a>
+        <a href="https://imaginary-blood-7556.netlify.app/" target="_blank" className='project-deployed-link'>
+        <button >Live Site</button>
+        </a>
+        </div>
       </div>
       <div className='project-card'>
-        <img src="" alt="" />
-        <h2 className='project-title'>Project2</h2>
-        <p className='project-description'></p>
-        <p className='project-tech-stack' ></p>
-        <p className='project-github-link'></p>
-        <p className='project-deployed-link'></p>
+        <img src={mountaineer} alt="" />
+        <div className='project-details'>
+        <h2 className='project-title'>Mountaineer</h2>
+        <p className='project-description'>Mountaineer is a clone of an e commerce website Eastern Mountain Sports which sells products related to adventure sports</p>
+        <div className='project-tech-stack'>
+        <h1>Tech Stack:-</h1>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+        </div>
+        <a href="https://github.com/SankethGaniga95/timely-river-8283" className='project-github-link' target='_blank'>
+        <button >GitHub</button>
+        </a>
+        <a href="https://timely-river-8283.netlify.app/" target="_blank" className='project-deployed-link'>
+        <button >Live Site</button>
+        </a>
+        </div>
       </div>
       <div className='project-card'>
-        <img src="" alt="" />
-        <h2 className='project-title'>Project3</h2>
-        <p className='project-description'></p>
-        <p className='project-tech-stack' ></p>
-        <p className='project-github-link'></p>
-        <p className='project-deployed-link'></p>
+        <img src={kiranawaale} alt="" />
+        <div className='project-details'>
+        <h2 className='project-title'>Kiranawaale</h2>
+        <p className='project-description'>About
+Kiranawaale is an e commerce website which sells products related to kitchen ware</p>
+        <div className='project-tech-stack'>
+        <h1>Tech Stack:-</h1>
+        <p>HTML</p>
+        <p>CSS</p>
+        <p>JavaScript</p>
+        </div>
+        <a href="https://github.com/SankethGaniga95/flawless-vegetable-3066" target='_blank' className='project-github-link'>
+        <button >GitHub</button>
+        </a>
+        <a href="https://poetic-bombolone-938874.netlify.app/" target="_blank" className='project-deployed-link'>
+        <button >Live Site</button>
+        </a>
+        </div>
       </div>
       <div className='project-card'>
-        <img src="" alt="" />
-        <h2 className='project-title'>Project4</h2>
-        <p className='project-description'></p>
-        <p className='project-tech-stack' ></p>
-        <p className='project-github-link'></p>
-        <p className='project-deployed-link'></p>
+        <img src={achiever} alt="" />
+        <div className='project-details'>
+        <h2 className='project-title'>Achiever's Avenue</h2>
+        <p className='project-description'>Achiever's Avenue is a learning platform for the aspirants of IIT and Medical Colleges. It offers online and offline coaching.</p>
+        <div className='project-tech-stack'>
+        <h1>Tech Stack:-</h1>
+        <p>React</p>
+        <p>Chakra UI</p>
+        <p>JSON server</p>
+        </div>
+        <a href="https://github.com/SankethGaniga95/imaginary-blood-7556" target='_blank' className='project-github-link'>
+        <button >GitHub</button>
+        </a>
+        <a href="https://imaginary-blood-7556.netlify.app/" target="_blank" className='project-deployed-link'>
+        <button >Live Site</button>
+        </a>
+        </div>
       </div>
     </div>
   )
